04642f2c8ebccbce9c14b83673ae07248dcf2273
aabi0044 <mabdulah596@hotmail.com>
2019-06-28 16:48:14 -0700

    front end

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl:NavController) { }
 
   ngOnInit() {
   }
-
+  back(){
+this.navCtrl.navigateBack(['']);
+  }
 }
