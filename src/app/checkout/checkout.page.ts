5827180002ce826794d6357e4f725b41b560bd2b
aabi0044 <mabdulah596@hotmail.com>
2019-06-28 19:22:19 -0700

    front end

--- src/app/checkout/checkout.page.ts
+++ src/app/checkout/checkout.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl:NavController) { }
 
   ngOnInit() {
   }
-
+  back(){
+    this.navCtrl.navigateBack(['/cart']);
+      }
 }
